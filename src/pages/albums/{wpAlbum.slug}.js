44672d8517949c1d2691044050a14f50f1cacc78
ValerieSleurs <sleurs.valerie@gmail.com>
2022-12-19 15:58:37 +0100

    AlbumPage (template): JSX structuur aangepast

--- src/pages/albums/{wpAlbum.slug}.js
+++ src/pages/albums/{wpAlbum.slug}.js
@@ -13,36 +13,40 @@ const AlbumPage = ({ data: { wpAlbum: { albumMeta: album, producers: { nodes: pr
 
   return (
     <Layout>
-      <div>
-        <h3>{album.artist}</h3>
-        <h4>{album.albumTitle} ({album.releaseYear})</h4>
-        <div>
-          {producers.map((producer, i) => (
-            <span key={i}>Producers: {producer.name} {i + 1 < producers.length && "- "}</span>
-          ))}
+      <section>
+        <div>         
+          <h2>{album.albumTitle} ({album.releaseYear})</h2>
+          <div>
+            <p>Produced by:</p>
+            {producers.map((producer, i) => (
+              <span key={i}>{producer.name} {i + 1 < producers.length && "- "}</span>
+            ))}
+          </div>
+          <p>Artist: {album.artist}</p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: album.description,
+            }}
+          />
+          <p>Album type: {album.albumType}</p>
+          <p>Record label: {album.recordLabel}</p>
+          <p>Length: {album.length}</p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: album.tracklisting,
+            }}
+          />
         </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: album.description,
-          }}
-        />
-        <p>Album type: {album.albumType}</p>
-        <p>Record label: {album.recordLabel}</p>
-        <p>Length: {album.length}</p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: album.tracklisting,
-          }}
-        />
         <div>
           <GatsbyImage className={albumCover} image={image} alt={album.albumCover.altText} />
         </div>
-        <div>
-          <h2>Discover {album.videoTitle}</h2>
-          <iframe className={iframe} width="560" height="315" src={album.videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>
-        </div>
-      </div>
-    </Layout>
+      </section>
+
+      <section>
+        <h2>Discover {album.videoTitle}</h2>
+        <iframe className={iframe} width="560" height="315" src={album.videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>
+      </section>
+    </Layout >
   )
 }
 
